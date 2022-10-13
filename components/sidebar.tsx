0358205fdd8b827101934304d9ea6b2a4b797765
hui lin <jane42242002@gmail.com>
2022-10-14 00:23:48 +0800

    feature: install lodash-es and sidebar mock data

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,30 +1,48 @@
+import _ from "lodash";
+
 const Sidebar = () => {
-  const data = [
+  const categories = [
     {
-      category: { id: "1", name: "娛樂" },
+      id: "1",
+      name: "娛樂",
       boards: [
         { id: "1", name: "電影" },
         { id: "2", name: "漫畫" },
       ],
     },
     {
-      category: { id: "2", name: "讀書" },
+      id: "2",
+      name: "讀書",
       boards: [
         { id: "1", name: "數學" },
         { id: "2", name: "英文" },
       ],
     },
     {
-      category: { id: "2", name: "讀書" },
+      id: "3",
+      name: "時尚",
       boards: [
-        { id: "1", name: "數學" },
-        { id: "2", name: "英文" },
+        { id: "1", name: "美妝" },
+        { id: "2", name: "穿搭" },
       ],
     },
   ];
+  console.log(categories);
   return (
     <div>
-      <p>分類</p>
+      <h2>分類</h2>
+      {_.map(categories, (category) => {
+        return (
+          <div>
+            <h3>{category.name}</h3>
+            <ul>
+              {_.map(category.boards, (board) => {
+                return <li>{board.name}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
