a10714d978c51049fa685184d8c17244266538c9
Jane <jane42242002@gmail.com>
2022-10-16 01:13:22 +0800

    refactor: extract rooms component

--- components/hall.tsx
+++ components/hall.tsx
@@ -1,24 +1,7 @@
 import _ from "lodash";
+import Rooms from "./rooms";
 
 const Hall = () => {
-  const rooms = [
-    { id: "1", name: "94愛電影", description: "大家快來~" },
-    { id: "2", name: "台中揪電影", description: "大家快來~" },
-    { id: "3", name: "沙丘讚讚", description: "大家快來~" },
-  ];
-  return (
-    <table>
-      <tbody>
-        {_.map(rooms, (room) => {
-          return (
-            <tr key={room.id}>
-              <td>{room.name}</td>
-              <td>{room.description}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+  return <Rooms />;
 };
 export default Hall;
